1c95ac3c42060beeb13636562e9acfcfa3cba340
chans <papajuju123@naver.com>
2021-05-01 15:11:54 +0900

    BE #2 : homr, new

--- CRUDproject/settings.py
+++ CRUDproject/settings.py
@@ -10,10 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os, json
+from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets_term=secrets):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets_term[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg) from KeyError
+
+SECRET_KEY = get_secret("SECRET_KEY")
+
 
 
 # Quick-start development settings - unsuitable for production
